7bc1e8984d6555a5af2c9d5094a7c035d93a5fcf
aurelia ritter <aureliaritter@gmail.com>
2023-05-06 09:13:29 +0200

    rec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+posts = []
+
+10.times do
+  posts << Article.create([{ title: Faker::Company.industry, content: Faker::Quote.yoda }])
+end
